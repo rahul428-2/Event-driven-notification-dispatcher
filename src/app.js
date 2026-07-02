daa9b69e928e65cac617b3b370c96f5fa9e6be25
rahul <prahul2005@gmail.com>
2026-07-02 12:37:52 +0530

    fix: enable CORS for cross-origin frontend requests

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
 app.use(express.json());
 app.use('/api/v1', eventRoutes);
 
